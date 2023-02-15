56c1fe00b01ee76c1f783dbd721c23d2733e871d
Yan <yan.bocharovv@gmail.com>
2023-02-15 15:25:03 +0300

    use useState method on project

--- src/App.js
+++ src/App.js
@@ -10,16 +10,16 @@ import "./scss/app.scss";
 
 function App() {
   return (
-    <div class="wrapper">
+    <div className="wrapper">
       <Header />
-      <div class="content">
-        <div class="container">
-          <div class="content__top">
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
             <Categories />
             <Sort />
           </div>
-          <h2 class="content__title">Все пиццы</h2>
-          <div class="content__items">
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
             <PizzaBlock title="Мексиканская" price="1000"/>
             <PizzaBlock title='Миланская' price="799"/>
             <PizzaBlock title='Русская' price="799"/>
